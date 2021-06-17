679f4946c3f0c0720f4fb04e8ddf9894e5666413
borislavstoychev <stoy4ew@gmail.com>
2021-06-17 22:48:32 +0300

    Unit and Integration Testing on Project #Todo creat test

--- Unit and Integration Testing/tests/views/test_create.py
+++ Unit and Integration Testing/tests/views/test_create.py
@@ -2,6 +2,8 @@ from django.db import DataError
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from lost_and_found_app.models import Object, Post
+
 
 class TestCreate(TestCase):
     def setUp(self):
@@ -45,3 +47,24 @@ class TestCreate(TestCase):
             response = self.test_client.post(reverse('create'), data=data_obj)
         except DataError:
             self.assertTemplateUsed('post_creat.html')
+
+    def test_postEdit_shouldRedirectToIndex(self):
+        data_obj = {
+            'name': 'Bobby',
+            'image': 'https://media.wired.com/photos/5f401ecca25385db776c0c46/master/pass/Gear-How-to-Apple-ios-13-home-screen-iphone-xs-06032019_big_large_2x.jpg',
+            'width': 5,
+            'height': 6,
+            'weight': 8,
+        }
+        obj = Object(**data_obj)
+        obj.save()
+        all_posts = (
+            Post(title='phone', description='broken', author_phone='0878799823', author_name='Bobby', object=obj),
+            Post(title='phone2', description='new', author_phone='0878799823', author_name='Borcho', object=obj),
+        )
+        [post.save() for post in all_posts]
+        data = all_posts[0].id
+        response = self.test_client.post(reverse(f'edit:{data}'), pk=data)
+        self.assertTemplateUsed(response, 'index.html')
+        form = response.context['post_form']
+        self.assertIsNotNone(form)
